e7a19769c47cfa81aff97ba9fbef165338f2e610
ellie-chorus <112037285+ellie-chorus@users.noreply.github.com>
2023-05-13 21:37:04 -0700

    Update selectionring.js

--- selectionring.js
+++ selectionring.js
@@ -2,11 +2,11 @@ function setRing() {
     $selection = document.getElementById('selection');
     $clicks = parseInt($selection.getAttribute('value')) + 1;
     $selection.setAttribute('value', $clicks);
-    if (clicks % 3 == 0) {
-        $selection.className = "very-important";
+    if ($clicks % 3 == 0) {
+        $selection.className = 'very-important';
     } else if ($clicks % 3 == 1) {
-        $selection.className = "less-important";
+        $selection.className = 'less-important';
     } else if ($clicks % 3 == 2) {
-        $selection.className = "important";
+        $selection.className = 'important';
     }
 }
