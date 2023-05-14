6282532388809a3b371c33e6d0f2898127159733
ellie-chorus <112037285+ellie-chorus@users.noreply.github.com>
2023-05-13 21:32:36 -0700

    Add files via upload

---
+++ selectionring.js
@@ -0,0 +1,12 @@
+function setRing() {
+    $selection = document.getElementById('selection');
+    $clicks = parseInt($selection.getAttribute('value')) + 1;
+    $selection.setAttribute('value', $clicks);
+    if (clicks % 3 == 0) {
+        $selection.className = "very-important";
+    } else if ($clicks % 3 == 1) {
+        $selection.className = "less-important";
+    } else if ($clicks % 3 == 2) {
+        $selection.className = "important";
+    }
+}
