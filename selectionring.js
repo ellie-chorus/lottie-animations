dc9b33e6a77090f9cf94cc3b7a50a59829ceaeaf
ellie-chorus <112037285+ellie-chorus@users.noreply.github.com>
2023-05-13 21:39:50 -0700

    Update selectionring.js

--- selectionring.js
+++ selectionring.js
@@ -4,9 +4,12 @@ function setRing() {
     $selection.setAttribute('value', $clicks);
     if ($clicks % 3 == 0) {
         $selection.className = 'very-important';
+        console.log('first if statement ran');
     } else if ($clicks % 3 == 1) {
         $selection.className = 'less-important';
+        console.log('second if statement ran');
     } else if ($clicks % 3 == 2) {
         $selection.className = 'important';
+        console.log('third if statement ran');
     }
 }
