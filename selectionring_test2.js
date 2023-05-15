6a9426ecc84575e02ebbd5ba1eb77922e4b7dd63
ellie-chorus <112037285+ellie-chorus@users.noreply.github.com>
2023-05-15 13:32:25 -0700

    Add files via upload

---
+++ selectionring_test2.js
@@ -0,0 +1,17 @@
+function setRing() {
+    $selection = document.getElementById('selection');
+    $clicks = parseInt($selection.getAttribute('value')) + 1;
+    $selection.setAttribute('value', $clicks);
+    $rabbit = document.getElementById('rabbit_form');
+    $rabbit.setAttribute('value', $clicks);
+    if ($clicks % 3 == 0) {
+        $selection.className = 'very-important';
+        console.log('first if statement ran');
+    } else if ($clicks % 3 == 1) {
+        $selection.className = 'less-important';
+        console.log('second if statement ran');
+    } else if ($clicks % 3 == 2) {
+        $selection.className = 'important';
+        console.log('third if statement ran');
+    }
+}
